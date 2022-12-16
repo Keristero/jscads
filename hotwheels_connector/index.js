78f0e4f669875d52e3f5f9d638c99b09315cc1a5
Keristero <keristero@gmail.com>
2022-12-16 14:45:57 +1300

    v3 version, works well

--- hotwheels_connector/index.js
+++ hotwheels_connector/index.js
@@ -45,8 +45,8 @@ const catch_dimensions = {
 
 const base_center = [0,0,(base_dimensions.height/2)]
 
-const left_catch_center = [-15.3,0,base_dimensions.height+(catch_dimensions.height/2)]
-const right_catch_center = [15.3,0,base_dimensions.height+(catch_dimensions.height/2)]
+const left_catch_center = [-15.8,0,base_dimensions.height+(catch_dimensions.height/2)]
+const right_catch_center = [15.8,0,base_dimensions.height+(catch_dimensions.height/2)]
 
 const top_slider_center = [0,-((base_dimensions.width/2)+(slider_dimensions.width/2)),(base_dimensions.height-slider_dimensions.height/2)]
 const bottom_slider_center = [0,(base_dimensions.width/2)+(slider_dimensions.width/2),(base_dimensions.height-slider_dimensions.height/2)]
@@ -56,7 +56,7 @@ const right_hole_center = [(base_dimensions.length/3.3),0,base_dimensions.height
 
 const main = () => {
     const base = cuboid({size:[base_dimensions.length,base_dimensions.width,base_dimensions.height],center:base_center})
-    const middle_bridge = cuboid({size:[base_dimensions.length,4,base_dimensions.height/2],center:[0,0,(base_dimensions.height/4)]})
+    const middle_bridge = cuboid({size:[base_dimensions.length,6.5,base_dimensions.height/2],center:[0,0,(base_dimensions.height/4)]})
     const top_slider = cuboid({size:[base_dimensions.length,slider_dimensions.width,slider_dimensions.height],center:top_slider_center})
     const bottom_slider = cuboid({size:[base_dimensions.length,slider_dimensions.width,slider_dimensions.height],center:bottom_slider_center})
 
@@ -65,23 +65,26 @@ const main = () => {
     const right_catch_top = roundedCylinder({center:right_catch_center,height: catch_dimensions.height, radius: catch_dimensions.diameter/2,segments:32,roundRadius:0.3})
 
     const middle_button_base = cylinder({center:[base_center[0],base_center[0],(base_dimensions.height+(button_dimensions.height/2))/2],height: base_dimensions.height+(button_dimensions.height/2), radius: button_dimensions.diameter/2,segments:32})
-    const left_catch_base = cylinder({center:[-15.3,0,(base_dimensions.height/2)+(catch_dimensions.height/2)],height: (base_dimensions.height)+(catch_dimensions.height/2), radius: catch_dimensions.diameter/2,segments:32})
-    const right_catch_base = cylinder({center:[15.3,0,(base_dimensions.height/2)+(catch_dimensions.height/2)],height: (base_dimensions.height)+(catch_dimensions.height/2), radius: catch_dimensions.diameter/2,segments:32})
+    const left_catch_base = cylinder({center:[-15.8,0,(base_dimensions.height/2)+(catch_dimensions.height/2)],height: (base_dimensions.height)+(catch_dimensions.height/2), radius: catch_dimensions.diameter/2,segments:32})
+    const right_catch_base = cylinder({center:[15.8,0,(base_dimensions.height/2)+(catch_dimensions.height/2)],height: (base_dimensions.height)+(catch_dimensions.height/2), radius: catch_dimensions.diameter/2,segments:32})
 
     let middle_button = union(middle_button_base,middle_button_top)
+    let radius = button_dimensions.diameter*2
+    let middle_button_concave_cutter = sphere({center:[0,0,base_dimensions.height+radius+(button_dimensions.height/2)],radius:radius})
+    middle_button = subtract(middle_button,middle_button_concave_cutter)
 
     let left_catch = union(left_catch_base,left_catch_top)
 
     let left_catch_cutter = cuboid({size:[5,5,5],center:[0,0,0],height: base_dimensions.height})
     left_catch_cutter = rotateY(1.2,left_catch_cutter)
-    left_catch_cutter = translate([-16.3,0,3+(base_dimensions.height/2)+(catch_dimensions.height)],left_catch_cutter)
+    left_catch_cutter = translate([-16.8,0,3+(base_dimensions.height/2)+(catch_dimensions.height)],left_catch_cutter)
     left_catch = subtract(left_catch,left_catch_cutter)
 
     let right_catch = union(right_catch_base,right_catch_top)
 
     let right_catch_cutter = cuboid({size:[5,5,5],center:[0,0,0],height: base_dimensions.height})
     right_catch_cutter = rotateY(-1.2,right_catch_cutter)
-    right_catch_cutter = translate([16.3,0,3+(base_dimensions.height/2)+(catch_dimensions.height)],right_catch_cutter)
+    right_catch_cutter = translate([16.8,0,3+(base_dimensions.height/2)+(catch_dimensions.height)],right_catch_cutter)
     right_catch = subtract(right_catch,right_catch_cutter)
 
     const rounded_base = roundedCuboid({size:[base_dimensions.length,base_dimensions.width+(slider_dimensions.width*2),base_dimensions.height*20],center:base_center,roundRadius:12})
